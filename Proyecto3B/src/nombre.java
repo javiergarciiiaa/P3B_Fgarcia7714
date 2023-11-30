8ae8c63f3d914a445cd23b2b33a71036fa243ef8
javiergarciiiaa <151049561+javiergarciiiaa@users.noreply.github.com>
2023-11-30 16:02:28 +0100

    Update nombre.java

--- Proyecto3B/src/nombre.java
+++ Proyecto3B/src/nombre.java
@@ -3,7 +3,7 @@ public class nombre {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Francisco Javier García Martínez");
+		System.out.println("Francisco Javier García Martínez y estoy llegando al final");
 	}
 
 }
